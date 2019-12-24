08db3fecfad5128edaaeda552db5592fa1298b16
Thomas Helton <thomas@heltonfamily.info>
2019-12-24 13:32:50 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 const Discord = require('discord.js')
+
 const client = new Discord.Client()
+
+
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
+  if (msg.content === 'c!help') {
+    msg.channel.send(`${msg.author}, Check your DM's!`);
+    //Embeded Message for help
+    msg.author.send({embed: {
+            color: 3447003,
+            "title": "Commands",
+            "author": {
+                "name": "Burger Boi",
+                "icon_url": "https://i.imgur.com/1b0ZkPG.jpg"
+            },
+            "footer": {
+                "text": "Created by SpaceRat#6734. DM me for any info/updates",
+                "icon_url": "https://cdn.discordapp.com/avatars/603395191054925835/589a3cfc44ee43ac1abd62a05cac2cc8.png?size=128"
+            },
+            "fields": [
+                {
+                    "name": "c!help",
+                    "value": "Displays all the commands.",
+                    "inline": false
+                },
+                {
+                    "name": "c!ping",
+                    "value": "Pong.",
+                    "inline": false
+                }
+            ]
+        }
+  });
+
   }
 })
 
-client.login(process.env.BOT_TOKEN);
+//rest of code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('NjU5MDQ5MjA0MDA5NjY0NTEz.XgJOAA.51PCbuVhRmQBgNafwwcbunnueT4')
